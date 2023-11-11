b5200c4d50020692782b5494f3f0bdd2fbf600d3
Hugh Carroll <hughie@carrollonline.co.uk>
2023-11-11 19:32:39 +0000

    Get output the same as the bash birthdayrem.

--- python/tooyoo.py
+++ python/tooyoo.py
@@ -22,6 +22,13 @@ class Tooyoo:
         for line in lines:
             line = line.replace("\n", "")
             person = self.parseLine(line)
+            person['age'] = self.calcAge(person)
+
+            if len(person['deathDate']) > 0:
+                person['next'] = "-"
+            else:
+                person['next'] = person['age'] + 1
+
             self.people.append(person)
 
         self.reorder()
@@ -38,15 +45,17 @@ class Tooyoo:
             "month": birthday[1],
             "day": birthday[2],
             "name": pieces[1].strip(),
-            "age": self.calcAge(line),
+            "birthday": birthday,
         }
 
-        try:
+        if len(pieces) > 2:
             deathDate = pieces[2].split('-')
+            person['deathDate'] = deathDate
             person["dYear"] = deathDate[0]
             person["dMonth"] = deathDate[1]
             person["dDay"] = deathDate[2]
-        except:
+        else:
+            person['deathDate'] = ""
             person["dYear"] = ""
             person["dMonth"] = ""
             person["dDay"] = ""
@@ -54,16 +63,18 @@ class Tooyoo:
         return person
 
     def output(self, people):
-        print("yyyy-mm-dd {:>10} AGE".format("NAME"))
-        print("-------------------------")
+        print("yyyy-mm-dd {:>10} {:>8} {:>8}".format(
+            "NAME", "AGE-NOW", "NEXT"))
+        print("---------------------------------------")
 
         for person in people:
-            print("{}-{}-{} {:>10} {}".format(
+            print("{}-{}-{} {:>10} {:>8} {:>8}".format(
                 person["year"],
                 person["month"],
                 person["day"],
                 person["name"],
                 person["age"],
+                person["next"],
                 )
             )
 
@@ -90,19 +101,39 @@ class Tooyoo:
         return
 
     def calcAge(self, person):
-        print(person)
-        return 0
-        # calcYear = self.today.year
-        #
-        # if int(person['month']) > self.today.month:
-        #     calcYear -= 1
-        # elif int(person['month']) == self.today.month:
-        #     if int(person['day']) > self.today.day:
-        #         calcYear -= 1
-        #
-        # age = calcYear - int(person['year'])
-        #
-        # return age
+        calcYear = self.today.year
+        birthYear = int(person['year'])
+        birthMonth = int(person['month'])
+        birthDay = int(person['day'])
+
+        if len(person['dYear']) > 0:
+            alive = False
+            deathYear = int(person['dYear'])
+            deathMonth = int(person['dMonth'])
+            deathDay = int(person['dDay'])
+        else:
+            alive = True
+
+        if alive is True:
+            if birthMonth > self.today.month:
+                calcYear -= 1
+            elif birthMonth == self.today.month:
+                if birthDay > self.today.day:
+                    calcYear -= 1
+
+            age = calcYear - birthYear
+        else:
+            if deathMonth > birthMonth:
+                age = deathYear - birthYear
+            elif deathMonth == birthMonth:
+                if deathDay > birthDay:
+                    age = deathYear - birthYear
+                else:
+                    age = (deathYear - birthYear) - 1
+            else:
+                age = (deathYear - birthYear) - 1
+
+        return age
 
 
 if __name__ == "__main__":
